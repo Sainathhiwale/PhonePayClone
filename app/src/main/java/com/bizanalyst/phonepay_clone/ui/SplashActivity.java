e66351780c46f6c7a86455ef30d5afac20ed9e79
Sainathhiwale <sainathhiwale2@gmail.com>
2018-06-20 02:17:58 +0530

    Initial commit

---
+++ app/src/main/java/com/bizanalyst/phonepay_clone/ui/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.bizanalyst.phonepay_clone.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bizanalyst.phonepay_clone.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
