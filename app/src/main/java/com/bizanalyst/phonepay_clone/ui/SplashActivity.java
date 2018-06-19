55f0e35fa6bb476ca5b583bf207a287135c7b08e
Sainathhiwale <sainathhiwale2@gmail.com>
2018-06-20 03:11:18 +0530

    splash activity and handler is added

--- app/src/main/java/com/bizanalyst/phonepay_clone/ui/SplashActivity.java
+++ app/src/main/java/com/bizanalyst/phonepay_clone/ui/SplashActivity.java
@@ -1,8 +1,12 @@
 package com.bizanalyst.phonepay_clone.ui;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bizanalyst.phonepay_clone.MainActivity;
 import com.bizanalyst.phonepay_clone.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,5 +15,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler mHandler = new Handler(Looper.getMainLooper());
+        mHandler.postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }, 1500);
     }
 }
