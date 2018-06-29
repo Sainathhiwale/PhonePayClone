052b435580f6d1021e7faa3e7e5c90afe27dc6dd
Sainathhiwale <sainathhiwale2@gmail.com>
2018-06-30 00:50:26 +0530

    offers model class getter and setter method field is defined

--- app/src/main/java/com/bizanalyst/phonepay_clone/model/OffersModel.java
+++ app/src/main/java/com/bizanalyst/phonepay_clone/model/OffersModel.java
@@ -1,4 +1,27 @@
 package com.bizanalyst.phonepay_clone.model;
 
 public class OffersModel {
+
+    private int image;
+    private String offer_on;
+    private String offer_details;
+
+    public OffersModel(int image, String offer_on, String offer_details) {
+        this.image = image;
+        this.offer_on = offer_on;
+        this.offer_details = offer_details;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getOffer_on() {
+        return offer_on;
+    }
+
+    public String getOffer_details() {
+        return offer_details;
+    }
+
 }
