9aef3d4c612b3aa54b5ba0c41a0faaa3b6596725
Sainathhiwale <sainathhiwale2@gmail.com>
2018-06-30 00:51:44 +0530

    transcation model class getter and setter method field is defined

--- app/src/main/java/com/bizanalyst/phonepay_clone/model/TransactionModel.java
+++ app/src/main/java/com/bizanalyst/phonepay_clone/model/TransactionModel.java
@@ -1,4 +1,39 @@
 package com.bizanalyst.phonepay_clone.model;
 
 public class TransactionModel {
+    private int image_transaction_type;
+    private String transaction_date, transaction_type, transaction_merchant, transaction_amount, transaction_credited_debited;
+
+    public TransactionModel(int image_transaction_type, String transaction_date, String transaction_type, String transaction_merchant, String transaction_amount, String transaction_credited_debited) {
+        this.image_transaction_type = image_transaction_type;
+        this.transaction_date = transaction_date;
+        this.transaction_type = transaction_type;
+        this.transaction_merchant = transaction_merchant;
+        this.transaction_amount = transaction_amount;
+        this.transaction_credited_debited = transaction_credited_debited;
+    }
+
+    public int getImage_transaction_type() {
+        return image_transaction_type;
+    }
+
+    public String getTransaction_date() {
+        return transaction_date;
+    }
+
+    public String getTransaction_type() {
+        return transaction_type;
+    }
+
+    public String getTransaction_merchant() {
+        return transaction_merchant;
+    }
+
+    public String getTransaction_amount() {
+        return transaction_amount;
+    }
+
+    public String getTransaction_credited_debited() {
+        return transaction_credited_debited;
+    }
 }
