a31529092e34681ed571865cbd1ec5e30f6e9dbd
Sainathhiwale <sainathhiwale2@gmail.com>
2026-08-27 14:53:02 +0530

    unit and ui testing file is updated

--- app/src/test/java/com/bizanalyst/phonepay_clone/ExampleUnitTest.kt
+++ app/src/test/java/com/bizanalyst/phonepay_clone/ExampleUnitTest.kt
@@ -1,17 +1,16 @@
-package com.bizanalyst.phonepay_clone;
+package com.bizanalyst.phonepay_clone
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.junit.Test
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-public class ExampleUnitTest {
+class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
     }
 }
